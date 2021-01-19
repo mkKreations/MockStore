d7c7a21483bf780a1d2abc0d0e373f14de04df87
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-01-19 16:38:17 -0500

    Set up tab bar controller/item for rootVC

--- MockStore/MockStore/AppDelegate.swift
+++ MockStore/MockStore/AppDelegate.swift
@@ -14,15 +14,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// instantiate window
 		window = UIWindow(frame: UIScreen.main.bounds)
+	
+		// create tab bar item for AppsVC
+		let item = UITabBarItem()
+		item.title = "Apps"
+		item.image = UIImage(systemName: "square.stack.3d.up.fill")
 		
-		// rootVC & navVC setup
+		// instantiate AppsVC and set tabBarItem & navController
 		let rootVC = AppsViewController()
-		let navVC = UINavigationController(rootViewController: rootVC)
+		rootVC.tabBarItem = item
+		let rootNavVC = UINavigationController(rootViewController: rootVC)
 		
-		window?.rootViewController = navVC
+		// set up tabBarController
+		let tabBarController = UITabBarController()
+		tabBarController.viewControllers = [rootNavVC]
+		tabBarController.selectedIndex = 0
+		
+		window?.rootViewController = tabBarController
 		window?.makeKeyAndVisible()
 		
 		return true
 	}
-
+		
 }
