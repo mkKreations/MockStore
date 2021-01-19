dc5631b4363c99c39dc0bde731f2a65e00897baa
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-01-19 16:18:29 -0500

    Set up large nav bar title and divider

--- MockStore/MockStore/Controllers/AppsViewController.swift
+++ MockStore/MockStore/Controllers/AppsViewController.swift
@@ -8,11 +8,34 @@
 import UIKit
 
 class AppsViewController: UIViewController {
+	// TODO: will be removed when we add cells
+	private let navBarDivider: UIView = UIView(frame: .zero)
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+
+		// nav bar setup
+		title = "Apps"
+		navigationController?.navigationBar.prefersLargeTitles = true
+		navigationController?.navigationBar.isTranslucent = false
 		
-		view.backgroundColor = .cyan
+		// view background
+		view.backgroundColor = .black
+		
+		// TODO: will be removed when we add cells
+		navBarDividerSetUp()
+	}
+	
+	// TODO: will be removed when we add cells
+	private func navBarDividerSetUp() {
+		navBarDivider.translatesAutoresizingMaskIntoConstraints = false
+		navBarDivider.backgroundColor = .darkGray
+		view.addSubview(navBarDivider)
+		
+		navBarDivider.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12.0).isActive = true
+		navBarDivider.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12.0).isActive = true
+		navBarDivider.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+		navBarDivider.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
 	}
 	
 }
