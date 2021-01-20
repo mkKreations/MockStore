0540f75a96ee8ca1b72d3028984da3f44a9f7e8c
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-01-19 19:06:41 -0500

    Added Section model

---
+++ MockStore/MockStore/Models/Section.swift
@@ -0,0 +1,15 @@
+//
+//  Section.swift
+//  MockStore
+//
+//  Created by Marcus on 1/19/21.
+//
+
+import Foundation
+
+struct Section: Decodable, Hashable {
+	let id: Int
+	let type: String
+	let title: String
+	let subtitle: String
+}
